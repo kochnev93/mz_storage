46a877e274f9b03454108899cc9b314e81875759
kochnev93 <kochnev93@mail.ru>
2022-11-07 23:27:54 +0300

    transfer modal

--- src/components/ui/Modal/Modal-transferProduct/MyModal-transferProduct.jsx
+++ src/components/ui/Modal/Modal-transferProduct/MyModal-transferProduct.jsx
@@ -4,10 +4,18 @@ import cx from 'classnames';
 // Hooks
 import authHeader from '../../../../services/auth-header';
 import { useAuth } from '../../../../hooks/use-auth';
+import useFetch from '../../../../hooks/useFetch';
 
 // Redux
 import { useDispatch, useSelector } from 'react-redux';
-import {setActiveTransfer, setErrorsTransfer, setMessageTransfer, setResetTransfer, setIsLoadingTransfer, setDefaultTransfer} from '../../../../features/modal/transfer-productSlice';
+import {
+  setActiveTransfer,
+  setErrorsTransfer,
+  setMessageTransfer,
+  setResetTransfer,
+  setIsLoadingTransfer,
+  setDefaultTransfer,
+} from '../../../../features/modal/transfer-productSlice';
 
 //Styles
 import styles from './MyModal-transferProduct.module.scss';
@@ -15,7 +23,7 @@ import styles from './MyModal-transferProduct.module.scss';
 // Components
 import MyDropdown from '../../Dropdown/MyDropdown.jsx';
 import Modal from '../MyModal2.jsx';
-
+import MyButton from '../../Buttons/ButtonSend.jsx';
 
 function ModalTransferProduct() {
   const dispatch = useDispatch();
@@ -27,9 +35,74 @@ function ModalTransferProduct() {
   const errors = useSelector((state) => state.modal_transfer_product.errors);
   const message = useSelector((state) => state.modal_transfer_product.message);
   const reset = useSelector((state) => state.modal_transfer_product.reset);
-  const isLoading = useSelector((state) => state.modal_transfer_product.isLoading);
+  const isLoading = useSelector(
+    (state) => state.modal_transfer_product.isLoading
+  );
 
-  console.log(product)
+  // Local State
+  const [warehouse, setWarehouse] = useState([]);
+  const [validationWarehouse, setValidationWarehouse] = useState(true);
+
+  const { fetchNow } = useFetch();
+
+  const validation = () => {
+    dispatch(setDefaultTransfer());
+    setValidationWarehouse(true);
+
+    if (warehouse.length === 0) {
+      dispatch(setMessageTransfer({ errors: true, message: 'Выберите склад' }));
+      setValidationWarehouse(false);
+      return false;
+    }
+
+    if (warehouse[0].title === product?.warehouse_title) {
+      dispatch(
+        setMessageTransfer({
+          errors: true,
+          message: 'Выбранный склад совпадает с текущим',
+        })
+      );
+      setValidationWarehouse(false);
+      return false;
+    }
+
+    let answerUser = confirm(
+      `Переместить товар ${product?.name} (${product?.sn}) на склад ${warehouse[0].title}?`
+    );
+
+    return answerUser ? true : false;
+  };
+
+  const transfer = async () => {
+    if (validation()) {
+      dispatch(setIsLoadingTransfer({ isLoading: true }));
+
+      let data = JSON.stringify({
+        warehouse: warehouse,
+        sn: product?.sn,
+      });
+
+      let requestOptions = {
+        method: 'POST',
+        body: data,
+      };
+
+      const result = await fetchNow(
+        'http://localhost:3001/api/transfer_product',
+        requestOptions
+      );
+
+      if (result.data) {
+        dispatch(setMessageTransfer({ message: result.data }));
+        setTimeout(() => {
+          dispatch(setIsLoadingTransfer({ isLoading: false }));
+        }, 100);
+      } else {
+        console.warn(result.error);
+        dispatch(setMessageTransfer({ errors: true, message: result.error }));
+      }
+    }
+  };
 
   return (
     <Modal
@@ -42,17 +115,50 @@ function ModalTransferProduct() {
       errors={errors}
       isLoading={isLoading}
     >
-      <p>Вы перемещате {product?.id}-{product?.name} с серийным номером <b>{product?.sn}</b> со склада {product?.warehouse_title}</p>
-      <h4>Куда переместить?</h4>
-      <MyDropdown
-            id="transfer_warehouse"
-            title="Склад"
-            placeholder="Выберите склад"
-            multiple={false}
-            validation={() => {}}
-            changeValue={() => {}}
-            url={'get_warehouse'}
-          />
+      <div className={styles.transfer_item}>
+        <h4>Выберите склад для перемещения</h4>
+        <MyDropdown
+          id="transfer_warehouse"
+          title="Склад"
+          placeholder="Выберите склад"
+          multiple={false}
+          validation={validationWarehouse}
+          changeValue={setWarehouse}
+          url={'get_warehouse'}
+        />
+      </div>
+
+      <div className={styles.transfer_item}>
+        <h4>Текущее расположение</h4>
+        <ul className={styles.product_prop}>
+          <li>
+            <span>ID: </span>
+            <span>{product?.id}</span>
+          </li>
+          <li>
+            <span>Наименование: </span>
+            <span>{product?.name}</span>
+          </li>
+          <li>
+            <span>SN: </span>
+            <span>{product?.sn}</span>
+          </li>
+          <li>
+            <span>Склад: </span>
+            <span>{product?.warehouse_title}</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.buttons}>
+        <MyButton
+          type="send"
+          action={transfer}
+          title="Переместить"
+          loadingTitle="Перемещаю"
+          loading={isLoading}
+        />
+      </div>
     </Modal>
   );
 }
